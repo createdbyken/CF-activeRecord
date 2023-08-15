2f533217ab03987c24f8d8503cf9093d0146dae9
Ken Sanchez <kgarcia@canceriq.com>
2023-08-15 14:13:15 -0600

    Changed annotations

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,8 +5,8 @@
 #  id         :bigint           not null, primary key
 #  title      :string
 #  code       :string
+#  price      :integer          default(0)
 #  stock      :integer          default(0)
-#  price      :decimal(10, 2)   default(0.0)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
