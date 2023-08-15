d30db825b39ec57d7f96061f2976648b576ed7bc
Ken Sanchez <kgarcia@canceriq.com>
2023-08-15 12:09:37 -0600

    Added Annotate gems and annotated product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id         :bigint           not null, primary key
+#  title      :string
+#  code       :string
+#  stock      :integer          default(0)
+#  price      :decimal(10, 2)   default(0.0)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Product < ApplicationRecord
 end
